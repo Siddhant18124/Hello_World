ac63d1ffd669f67092f40cb73e4edbb94bff5024
abid249 <92668396+abid249@users.noreply.github.com>
2021-10-30 23:01:58 +0530

    Create Circle.cpp

---
+++ Circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define PI 3.1415
+
+using namespace std;
+
+int main()
+{
+    
+    float r, area;
+    cout<<"enter the radius";
+    cin>>r;
+    area=PI*r*r;
+    cout<<area;
+    return 0;
+}
